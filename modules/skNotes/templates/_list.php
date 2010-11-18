8ad2880ef6675b6f8e4f2141691de8d2cad07c89
Colin Darie <colindarie@gmail.com>
2010-11-18 14:12:43 +0100

    Fixed id attribute of each note with note- prefix

--- modules/skNotes/templates/_list.php
+++ modules/skNotes/templates/_list.php
@@ -2,7 +2,7 @@
   <h4><?php echo __('Notes')?></h4>
   <ol class="notes-list">
     <?php foreach ($notes as $note): ?>
-      <li id="ref-<?php echo $note->getIndex() ?>" value="<?php echo $note->getIndex() ?>"><?php echo $note->getText() ?></li>
+      <li id="note-<?php echo $note->getIndex() ?>" value="<?php echo $note->getIndex() ?>"><?php echo $note->getText() ?></li>
     <?php endforeach ?>
   </ol>
 </div>
